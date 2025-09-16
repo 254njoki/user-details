3bd41665c1f9477e66767ce90620c466edf18368
254njoki <njokifidelis12@gmail.com>
2025-09-16 21:01:59 +0300

    Add files via upload

---
+++ user_Details.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>//pre-processor descriptive.scanf
+
+//main function
+int main(){
+    printf("Good morning:\n");
+    printf("Good morning too:\n");
+    int height;
+    printf("Enter your height:");
+    scanf("%d",& height);
+    printf("Your height is %d");
+    int phone_no;
+    
+    printf("\nEnter your phone no:");
+    scanf("%d", & phone_no);
+    printf("\nyour phone is %d");
+    
+    int weight;
+    printf("\nEnter your weight:");
+    scanf("%d",& weight);
+    printf("\n your weight is %d");
+    
+     
+    return 0;
+}
